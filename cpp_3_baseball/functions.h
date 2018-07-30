fb11ec34cf02e0505c7ff86368106e1e5583cf5b
SeonghwanKim13109 <reasonby13@naver.com>
2018-07-30 16:54:50 +0900

    사용할 함수를 선언한 헤더 파일 생성

---
+++ cpp_3_baseball/functions.h
@@ -0,0 +1,7 @@
+#pragma once
+void makeArr(int[], int);
+void shuffle(int[], int);
+void swap(int[], int, int);
+int findStrike(int[], int[], int);
+int findBall(int[], int[], int);
+void showLife(int);
